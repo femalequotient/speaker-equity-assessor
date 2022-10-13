6ba678e85afcaf255ffbfacc55016728a32553ae
Dan Schultz <slifty@gmail.com>
2022-10-13 16:56:00 -0400

    Update /speakers route
    We want the tool to exist behind a /speakers path, rather than /tally.
    
    Issue #7 Rename tally route with speakers

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,5 +1,5 @@
 import { redirect } from '@sveltejs/kit';
 
 export const load = () => {
-	throw redirect(302, '/tally');
+	throw redirect(302, '/speakers');
 };
--- tests/test.ts
+++ tests/test.ts
@@ -1,6 +1,6 @@
 import { expect, test } from '@playwright/test';
 
 test('index page has expected h1', async ({ page }) => {
-	await page.goto('/tally');
+	await page.goto('/speakers');
 	expect(await page.textContent('h1')).toBeDefined();
 });
