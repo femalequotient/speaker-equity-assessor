9e102d13e6a09d6a20d6418bd223fca30c4e6bcc
Dan Schultz <slifty@gmail.com>
2022-10-13 15:53:29 -0400

    Add page title
    Issue #12 Add favicon and page title

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,6 +4,10 @@
 	import ThemeLoader from '../lib/components/ThemeLoader.svelte';
 </script>
 
+<svelte:head>
+	<title>Speaker Equity Assessor™</title>
+</svelte:head>
+
 <ThemeLoader />
 <div id="container">
 	<slot />
